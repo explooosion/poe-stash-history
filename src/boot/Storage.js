00f096651f3cafa7ab5bc91e335b1be9b44b2bf2
Robby <ta7382@gmail.com>
2020-10-18 21:59:09 +0800

    Add stroage boot

---
+++ src/boot/Storage.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+export default class Storage {
+
+  static setItem(key, value) {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  static getItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  static removeItem(key) {
+    return localStorage.removeItem(key);
+  }
+
+  static clear() {
+    return localStorage.clear();
+  }
+
+  static hasIn(key) {
+    return _.isObject(Storage.getItem(key));
+  }
+}
