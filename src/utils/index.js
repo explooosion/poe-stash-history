e07d12903d89ec5373469c934b44d4ab44726ab9
Robby <ta7382@gmail.com>
2020-10-20 20:28:20 +0800

    Update util

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,22 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 
 /**
  * Replace url params to target value
  * @param {string} url Request Uul
  * @param {object} params Params
+ * @example id=:id&name=:name => id=1&name=Robby
  */
 export function replaceUrlParams(url = '', params = {}) {
-  const prefix = ':';
-  // { id: 1, name: 'Robby' } => { ':id': 1, ':name': 'Robby' }
-  const mapObj = Object.keys(params).reduce((acc, cur) => ({ ...acc, [`${prefix}${cur}`]: params[cur] }), {});
-  // /:id|:name/gi
-  const rexStr = Object.keys(mapObj).reduce((acc, cur) => [...acc, cur], []).join('|');
-  // id=:id&name=:name => id=1&name=Robby
-  const result = url.replace(new RegExp(rexStr, 'gi'), matched => mapObj[matched]);
-  return result;
+  if (_.isEmpty(params)) {
+    return url;
+  } else {
+    const prefix = ':';
+    // { id: 1, name: 'Robby' } => { ':id': 1, ':name': 'Robby' }
+    const mapObj = _.chain(params).keys(params).reduce((acc, cur) => ({ ...acc, [`${prefix}${cur}`]: params[cur] }), {}).value();
+    // /:id|:name/gi
+    const rexStr = _.chain(mapObj).keys(mapObj).reduce((acc, cur) => [...acc, cur], []).join('|').value();
+    // id=:id&name=:name => id=1&name=Robby
+    return _.replace(url, new RegExp(rexStr, 'gi'), matched => mapObj[matched]);
+  }
 }
 
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -1,10 +1,16 @@
 import { replaceUrlParams } from './index';
 
 describe('Test Utils', () => {
-  it('replaceUrlParams', () => {
+  it('replaceUrlParams match', () => {
     const urlResult = 'https://web.poe.garena.tw/character-window/get-characters?accountName=MyAccount';
     const url = process.env.REACT_APP_GET_CHARACTERS;
     const params = { accountName: 'MyAccount' };
     expect(replaceUrlParams(url, params)).toBe(urlResult);
+  });
+
+  it('replaceUrlParams not match', () => {
+    const url = process.env.REACT_APP_GET_CHARACTERS;
+    const params = {};
+    expect(replaceUrlParams(url, params)).toBe(url);
   });
 });
